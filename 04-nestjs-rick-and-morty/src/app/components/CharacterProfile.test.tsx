6dddd05471871d912eb4deda8a24b4bb6d95ff71
Felipe Acosta <felipegonac0@gmail.com>
2024-06-12 10:36:41 -0600

    fix: fixed test to expect a call from an API

--- 04-nestjs-rick-and-morty/src/app/components/CharacterProfile.test.tsx
+++ 04-nestjs-rick-and-morty/src/app/components/CharacterProfile.test.tsx
@@ -1,10 +1,10 @@
-import { expect, it, describe } from 'vitest'
+import { it, describe } from 'vitest'
 import { screen, render } from '@testing-library/react'
 import CharacterProfile from './CharacterProfile'
 
 describe('Character Component Test', () => {
-  it('should be rendered on the screen', () => {
+  it('should be rendered on the screen', async () => {
     render(<CharacterProfile id={'1'} />)
-    expect(screen.getByTestId('Character-profile')).toBeInTheDocument()
+    await screen.findByTestId('Character-profile')
   })
 })
